3cfa2259da864ef3a839280dc8e85719f5ac0771
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-02-18 04:01:14 -0500

    solution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,10 +13,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.status(200).json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+})
+
+app.listen(8800)
